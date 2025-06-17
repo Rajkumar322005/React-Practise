ec841ee058addd5a5eb5b39a7278eda152492c0d
Rajkumar322005 <sivasubaramanipkm@gmail.com>
2025-06-17 20:41:59 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Prop from './components/prop.jsx'
+import UserCart from './components/UserCart.jsx'
+const App = () => {
+  return (
+    <div>
+      {/* <Prop name = "Rajesh"></Prop> */}
+      <UserCart/>
+    </div>
+  )
+}
+
+export default App
