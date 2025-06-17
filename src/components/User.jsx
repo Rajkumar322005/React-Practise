d8de8d00851d6f97e585703f8a9b7f8fcdd35e75
Rajkumar322005 <sivasubaramanipkm@gmail.com>
2025-06-17 23:41:02 +0530

    Added User Profile Component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
-function User() {
+function User(props) {
   return (
-    <div>
-        Hello
+    <div className='card-container'>
+        <span className={props.online? "pro online" : "pro offline" }>{props.online?"ONLINE":"OFFLINE"}</span>
+        <img className = 'img'src={props.profile} alt="User" />
+        <h3>{props.name}</h3>
+        <h3>{props.city}</h3>
+        <p>{props.description}</p>
+        <div className='buttons'>
+            <button className = 'primary'>Message</button>
+            <button className='primary outline'>Following</button>
+        </div>
+        <div className='skills'>
+            <h6>SKILLS</h6>
+            <ul>
+              {props.skills.map((skill)=>(
+                <li>{skill}</li>
+              ))}
+            </ul>
+            
+        </div>
+        
     </div>
   )
 }
