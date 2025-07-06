f994d8d6bf2191feae7db7fb2a738a54bbb9c50b
Rajkumar322005 <sivasubaramanipkm@gmail.com>
2025-07-06 15:26:42 +0530

    About us Completed

--- src/components/Foodcart/About.jsx
+++ src/components/Foodcart/About.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import "./Header.css"; // Import the CSS file
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about-container">
+      <h1>About Us</h1>
+      <p>
+        Welcome to our food cart application! We are passionate about bringing
+        delicious food to your doorstep.
+      </p>
+      <p>
+        Our mission is to provide a seamless and enjoyable food ordering
+        experience.
+      </p>
+      <p>Thank you for choosing us!</p>
+      <p>
+        Contact us at:
+        <a href="mailto:support@Tvshow.com"> support@Tvshow.com</a>
+      </p>
+      <p>Follow us on social media:</p>
+      <ul>
+        <li>
+          <a href="https://www.facebook.com/foodcart">Facebook</a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com/foodcart">Twitter</a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/foodcart">Instagram</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default About
+export default About;
